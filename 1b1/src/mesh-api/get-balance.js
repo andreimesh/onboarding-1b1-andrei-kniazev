39b996ffe7c4a21806929ce19a00387e198bb4c7
Andrey Kniazev <andrei.kniazev@meshconnect.com>
2025-06-04 19:09:17 +0200

    Added configurable get balance

--- 1b1/src/mesh-api/get-balance.js
+++ 1b1/src/mesh-api/get-balance.js
@@ -1,9 +1,28 @@
 import { secret, baseUrl, getStoredPayload } from "./secret"
 
 
+/**
+ * @typedef {Object} Balance
+ * @property {number} buyingPower - The available buying power in the account.
+ * @property {number} cash - The cash available in the account.
+ * @property {number} cryptocurrencyBuyingPower - The available buying power for cryptocurrencies.
+ * @property {string} currencyCode - The currency code (e.g., "USD").
+ */
+
+/**
+ * @typedef {Object} BalancesResponse
+ * @property {Balance[]} balances - Array of balance objects for different currencies/accounts.
+ * @property {number} totalBuyingPowerUsdValue - The total buying power in USD.
+ * @property {number} totalCashUsdValue - The total cash value in USD.
+ */
+
+
+/**
+ * @returns {Promise<BalancesResponse>} A promise that resolves to the balances response object.
+ */
 export async function getBalance() {
   const authToken = getStoredPayload().accountTokens[0].accessToken;
-  console.log("Auth Token:", authToken);
+  const brokerType = getStoredPayload().brokerType;
   const options = {
     method: 'POST',
     headers: {
@@ -13,12 +32,11 @@ export async function getBalance() {
     },
     body: JSON.stringify({
       authToken: authToken,
-      type: "coinbase",
+      type: brokerType,
     })
   };
 
   const response = await fetch(`${baseUrl()}/api/v1/balance/get`, options);
   const jsonResponse = await response.json();
-  console.log(jsonResponse);
-
+  return jsonResponse.content;
 }
